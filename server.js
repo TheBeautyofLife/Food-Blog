e020d3263f87f0477f119260f813a6de5c9abab6
Stephanie Kirathe <stephaniekirathe@gmail.com>
2019-08-25 09:24:04 +0300

    package

--- server.js
+++ server.js
@@ -5,6 +5,15 @@ const port = process.env.PORT || 4000;
 
 const server = http.createServer(app);
 
+// Handle production
+if (process.env.NODE_ENV === 'production') {
+  // Static folder
+  app.use(express.static(__dirname + '/public/'));
+
+  // Handle SPA
+  app.get(/.*/, (req, res) => res.sendFile(__dirname + '/public/index.html'));
+}
+
 server.listen(port, () => {
     console.log(`server running on ${port}`)
 })
